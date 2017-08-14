00636ab569f6d34a03431ed86b0fa8a85c6e4d50
Joe Volpe <31015697+EvanVolpe@users.noreply.github.com>
2017-08-14 18:03:06 -0400

    Fixed CSS sheet and added new js file

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("script detected")
+function firstfocus() {
+	document.getElementsByName("userid")[0].focus();
+}
+function userid_validation(int, int2){
+	var inputValue = Number(document.getElementById('myId').value);
+}
+function username () console.log;
+var input = document.getElementById('name');
+console.log(input.value)
